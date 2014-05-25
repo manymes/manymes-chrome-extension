bb7278a74c3bf2b5687ce58c775636b12a5efa5a
David Neubauer <davidneub@gmail.com>
2014-05-25 21:31:51 +0200

    modified the manymes banner on active tab

--- app/scripts/view.js
+++ app/scripts/view.js
@@ -15,6 +15,7 @@ var manymes = window.manymes || {};
         $(this).on(this.EVENTS.STATE_CHANGED, this.onStateChanged);
         var that = this;
         this.$button.on('click', function(){
+            console.log(123);
             console.log(that.EVENTS.CHANGE_STATE);
             $(that).trigger(that.EVENTS.CHANGE_STATE, {
                 type: 'active',
