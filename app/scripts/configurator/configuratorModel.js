11f81d2946753899d8f90d69669903c35b8c3742
Joscha Probst <jprobst.mmt-b2012@fh-salzburg.ac.at>
2014-06-16 21:51:02 +0200

    avatars are stored in chrome.storage and read from it on load

--- app/scripts/configurator/configuratorModel.js
+++ app/scripts/configurator/configuratorModel.js
@@ -34,14 +34,34 @@ var manymes = window.manymes || {};
     };
 
     ConfiguratorModel.prototype.initActiveAvatarIndices = function(){
-        //DOTO - get them from localstorage
-        //
-        //MOCK function
-        this.activeAvatarIndices = [0,1,2];
-        this.allAvatars[this.activeAvatarIndices[0]].animation.start($('#avatar-0'));
-        this.allAvatars[this.activeAvatarIndices[1]].animation.start($('#avatar-1'));
-        this.allAvatars[this.activeAvatarIndices[2]].animation.start($('#avatar-2'));
+
+        var that = this;
+        chrome.storage.local.get('activeAvatarIndices', function (result) {
+            if($.isEmptyObject(result)){
+                console.log('empty');
+                chrome.storage.local.set({'activeAvatarIndices': JSON.stringify([0,1,2])}, function () {
+                    console.log('set');
+                    that.activeAvatarIndices = [0,1,2];
+                });
+            }else{
+                console.log('from local storage');
+                that.activeAvatarIndices = JSON.parse(result.activeAvatarIndices);
+            }
+            
+
+            that.allAvatars[that.activeAvatarIndices[0]].animation.start($('#avatar-0'));
+            that.allAvatars[that.activeAvatarIndices[1]].animation.start($('#avatar-1'));
+            that.allAvatars[that.activeAvatarIndices[2]].animation.start($('#avatar-2'));
+        });
     };
+
+    ConfiguratorModel.prototype.setActiveAvatarIndex = function(slot){
+        this.activeAvatarIndices[slot] = this.getPrevAvatar(this.activeAvatarIndices[slot]);
+        chrome.storage.local.set({'activeAvatarIndices': JSON.stringify(this.activeAvatarIndices)}, function () {
+            console.log('set active avatar index');
+        });
+    };
+
 
 
     ConfiguratorModel.prototype.writePermalinkToStorage = function(permalink){
@@ -96,14 +116,14 @@ var manymes = window.manymes || {};
 
     ConfiguratorModel.prototype.onPrevAvatar = function(event, slot, that){
         this.allAvatars[this.activeAvatarIndices[slot]].animation.stop();
-        this.activeAvatarIndices[slot] = that.getPrevAvatar(this.activeAvatarIndices[slot]);
+        this.setActiveAvatarIndex(slot);
         this.allAvatars[this.activeAvatarIndices[slot]].animation.start($('#avatar-' + slot));
         $(that).trigger(that.EVENTS.AVATAR_CHANGED, that.getPermalink());
     };
 
     ConfiguratorModel.prototype.onNextAvatar = function(event, slot, that){
         this.allAvatars[this.activeAvatarIndices[slot]].animation.stop();
-        this.activeAvatarIndices[slot] = that.getNextAvatar(this.activeAvatarIndices[slot]);
+        this.setActiveAvatarIndex(slot);
         this.allAvatars[this.activeAvatarIndices[slot]].animation.start($('#avatar-' + slot));
 
         $(that).trigger(that.EVENTS.AVATAR_CHANGED, that.getPermalink());
